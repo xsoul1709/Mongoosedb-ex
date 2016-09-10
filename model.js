aa02829951f92f88979e3325c86b1c74b73984cb
Pham Hoai Anh <anhph5@fsoft.com.vn>
2016-09-10 21:12:39 +0700

    task 1: design database

--- model.js
+++ model.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 var info = new Schema({
-
+    userName: String,
+    password: String
 });
 
 var qa = new Scheme({
-
+    question: String,
+    answer: []
 });
 
 var setting = new Schema({
-
+    percentPassed: Number,
+    numberOfQA: Number,
+    duration: Number
 });
